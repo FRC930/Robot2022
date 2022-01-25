3edaf77d92862f80e6135e15138e86cbe19fb96d
axonon <jmps2022@gmail.com>
2022-01-24 20:45:41 -0600

    Intake Pistons 20:42
    Created the IntakePistonSubsystem, EngageIntakePistonsCommand, and
    DisengageIntakePistonsCommand.
    There are two solenoids planned for intake.
    Engaged = down and vice versa.
    
    Default command and buttons still have to be set in RobotContainer.
    (no changes to RobotContainer have been made)

---
+++ Robot2022Code/src/main/java/frc/robot/subsystems/IntakePistonSubsystem.java
@@ -0,0 +1,70 @@
+//----- IMPORTS -----\\
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//----- CLASS -----\\
+/**
+ * <h3>Intake Piston Subsystem</h3>
+ * 
+ * Handles the pistons that raise and lower the intake.
+ */
+public class IntakePistonSubsystem extends SubsystemBase {
+
+    //----- SOLENOID(S) -----\\
+
+    private Solenoid solenoidOne;
+    private Solenoid solenoidTwo;
+
+    //----- CONSTRUCTOR -----\\
+    /**
+     * <h3>Intake Piston Subsystem</h3>
+     * 
+     * Handles the pistons that raise and lower the intake.
+     * 
+     * @param solenoidOneID ID for the first intake solenoid.
+     * @param solenoidTwoID ID for the second intake solenoid.
+     */
+    public IntakePistonSubsystem(int solenoidOneID, int solenoidTwoID) {
+        solenoidOne = new Solenoid(PneumaticsModuleType.REVPH, solenoidOneID);
+        solenoidTwo = new Solenoid(PneumaticsModuleType.REVPH, solenoidTwoID);
+    }
+
+        /**
+     * <h3>setIntakePistonState</h3>
+     * 
+     * Sets the shifter to the passed state
+     * 
+     * @param state the state to write to the piston
+     */
+    public void setIntakePistonState(boolean state) {
+        solenoidOne.set(state);
+        solenoidTwo.set(state);
+    }
+
+    /**
+     * <h3>getIntakeSolenoidOneState</h3>
+     * 
+     * Reads the state of the second intake piston
+     * 
+     * @return the piston state
+     */
+    public boolean getIntakeSolenoidOneState() {
+        return solenoidOne.get();
+    }
+
+    /**
+     * <h3>getIntakeSolenoidTwoState</h3>
+     * 
+     * Reads the state of the second intake piston
+     * 
+     * @return the piston state
+     */
+    public boolean getIntakeSolenoidTwoState() {
+        return solenoidTwo.get();
+    }
+
+}
