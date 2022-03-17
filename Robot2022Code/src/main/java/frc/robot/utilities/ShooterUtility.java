bb94174c940d948d12cddec8821bb64d6678906c
FrostBeast1 <frostbeast9@gmail.com>
2022-03-17 17:47:00 -0500

    Updates and Shooter Utility 17:46 Updated wpilib and other vendordeps. Finished the ShooterUtility math.

--- Robot2022Code/src/main/java/frc/robot/utilities/ShooterUtility.java
+++ Robot2022Code/src/main/java/frc/robot/utilities/ShooterUtility.java
@@ -1,29 +1,20 @@
 package frc.robot.utilities;
 
 /**
- * ShooterSubsystem
+ * ShooterUtility
  * Calculates how to get a shot in the hub
  */
 public class ShooterUtility {
-
-    // All measurements are in meters
-    private final double BALL_RADIUS = 0.12065;
-    private final double HUB_DIAMETER = 1.2192;
-    private final double HUB_HEIGHT = 2.6416;
-    private final double START_HEIGHT = 0.7366;
-    private final double HEIGHT_DIFFERENCE = HUB_HEIGHT - START_HEIGHT;
-    private static final double TOP_WHEEL_RADIUS = 0.0254;
-    private static final double BOTTOM_WHEEL_RADIUS = 0.0508;
-    private final double GRAVITY = 9.8;
 
     // Flag for singleton
     private static ShooterUtility lastInstance = null;
 
     /**
-     * Methods to get instance from singleton
-     * this one uses the default constructed object, this is the one to use when getting single instance
-     * If lastInstance doesnt exist create it
-     * @return
+     * <h3>getInstance</h3>
+     * Methods to get singleton instance.
+     * Creates one if never created.
+     * 
+     * @return the singleton object
      */
     public static ShooterUtility getInstance() {
         if (lastInstance == null) {
@@ -32,36 +23,145 @@ public class ShooterUtility {
         return lastInstance;
     }
 
+    // Used Desmos to plot out best-fit lines
+    // Link: https://www.desmos.com/calculator/mbuhfoziwi
+    // Constants for speed best-fit lines
+    // Y = MX + B
+    private double SPEED_M = 1.323076923;
+    private double TOP_SPEED_B = 65.48717949;
+    private double BOT_SPEED_B = 15.48717949;
+    // Constants for hood best-fit line
+    // Y = A(X^3) + B(X^2) + CX + D
+    private double HOOD_A = 0.13834842;
+    private double HOOD_B = -2.30271897;
+    private double HOOD_C = 15.14317395;
+    private double HOOD_D = -16.10579851;
+
     // Math variables
     private double launchAngle;
-    private double launchVelocity;
-    private double distanceToHub;
+    private double launchSpeedTop;
+    private double launchSpeedBottom;
 
     /**
      * Sets shooter math variables to -1
      */
     private ShooterUtility() {
         launchAngle = -1;
-        launchVelocity = -1;
-        distanceToHub = -1;
+        launchSpeedTop = -1;
+        launchSpeedBottom = -1;
     }
 
     /**
-     * Returns the velocity required to make the shot. Will return -1 if error
-     * occurs.
+     * <h3>setShooterDistance</h3>
+     * Sets the current shooter distance and calculates the shooter values.
      * 
-     * @return {@link #velocity} set in {@link #calculateVelocity}
+     * @param distance to the hub in meters
      */
-    public double getVelocity() {
-        return this.launchVelocity;
+    public void setShooterDistance(double distance) {
+        if (distance > 0 && metersToFeet(distance) <= 27) {
+            launchAngle = calculateHoodPos(distance);
+            launchSpeedTop = calculateTopSpeed(distance);
+            launchSpeedBottom = calculateBottomSpeed(distance);
+        } else {
+            launchAngle = -1;
+            launchSpeedTop = -1;
+            launchSpeedBottom = -1;
+        }
     }
 
     /**
-     * Returns whether the inner shot is possible
+     * <h3>calculateTopSpeed</h3>
+     * Returns the new speed for the top roller to make the shot.
      * 
-     * @return {@link #shotOutcome} set in {@link #calculateTrajectory}
+     * @param distance the distance from the hub
+     * @return the required speed in percent output
      */
-    public boolean getPossibleShot() {
-        return true;
+    private double calculateTopSpeed(double distance) {
+        if (metersToFeet(distance) >= 8) {
+            return (SPEED_M * distance + TOP_SPEED_B) / 100;
+        } else if (metersToFeet(distance) >= 6) {
+            return 0.76;
+        } else if (metersToFeet(distance) >= 2) {
+            return 0.74;
+        } else {
+            return 1.0;
+        }
+    }
+
+    /**
+     * <h3>calculateBottomSpeed</h3>
+     * Returns the new speed for the bottom roller to make the shot.
+     * 
+     * @param distance the distance from the hub
+     * @return the required speed in percent output
+     */
+    private double calculateBottomSpeed(double distance) {
+        if (metersToFeet(distance) >= 8) {
+            return (SPEED_M * distance + BOT_SPEED_B) / 100;
+        } else if (metersToFeet(distance) >= 6) {
+            return 0.26;
+        } else if (metersToFeet(distance) >= 2) {
+            return 0.24;
+        } else {
+            return 0.0;
+        }
+    }
+
+    /**
+     * <h3>calculateHoodPos</h3>
+     * Returns the new hood positiion to make the shot.
+     * 
+     * @param distance the distance from the hub
+     * @return the required position in degrees
+     */
+    private double calculateHoodPos(double distance) {
+        if (metersToFeet(distance) >= 19) {
+            return 30.0;
+        } else if (metersToFeet(distance) >= 8) {
+            return 28.44;
+        } else {
+            return HOOD_A * Math.pow(distance, 3) + HOOD_B * Math.pow(distance, 2) + HOOD_C * distance + HOOD_D;
+        }
+    }
+
+    /**
+     * <h3>getTopSpeed</h3>
+     * Returns the speed for the top roller to make the shot.
+     * 
+     * @return the required speed in percent output
+     */
+    public double getTopSpeed() {
+        return this.launchSpeedTop;
+    }
+
+    /**
+     * <h3>getBottomSpeed</h3>
+     * Returns the speed for the bottom roller to make the shot.
+     * 
+     * @return the required speed in percent output
+     */
+    public double getBottomSpeed() {
+        return this.launchSpeedBottom;
+    }
+
+    /**
+     * <h3>getHoodAngle</h3>
+     * Returns the angle required to make the shot.
+     * 
+     * @return the required angle in degrees
+     */
+    public double getHoodAngle() {
+        return this.launchAngle;
+    }
+
+    /**
+     * <h3>metersToFeet</h3>
+     * Converts distance meters to feet.
+     * 
+     * @param length in meters
+     * @return the length in feet
+     */
+    public double metersToFeet(double length) {
+        return 3.28084 * length;
     }
 }
