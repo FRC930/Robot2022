19a26ab0d85358fd64057e28bdf0738caa6863f2
wizardwarior101 <57240050+EdPilon@users.noreply.github.com>
2022-03-19 10:24:22 -0500

    + last push
    10:24

--- Robot2022Code/src/main/java/frc/robot/commands/autovisioncommands/HubAimCommand.java
+++ Robot2022Code/src/main/java/frc/robot/commands/autovisioncommands/HubAimCommand.java
@@ -1,11 +1,26 @@
 package frc.robot.commands.autovisioncommands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class HubAimCommand extends SequentialCommandGroup{
 
-    public HubAimCommand(DriveSubsystem dSubsystem){
-    new SequentialCommandGroup(new PigeonAimCommand(dSubsystem), new PhotonAimCommand(dSubsystem));
+    public HubAimCommand(DriveSubsystem driveSubsystem){
+        new SequentialCommandGroup(
+            new PigeonAimCommand(driveSubsystem), 
+            new PhotonAimCommand(driveSubsystem)
+        );
+    }
+
+    public HubAimCommand(
+        DriveSubsystem driveSubsystem, 
+        XboxController driverController, 
+        XboxController coDriverController
+    ) {
+        new SequentialCommandGroup(
+            new PigeonAimCommand(driveSubsystem), 
+            new PhotonAimCommand(driveSubsystem, driverController, coDriverController)
+        );
     }
 }
